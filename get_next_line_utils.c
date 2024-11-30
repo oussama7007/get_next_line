0cbfa2a9da58fa830a2f4ceb22365c9f8e881b60
Oussama Ait Si Hamou <oait-si-@e1r13p4.1337.ma>
2024-11-30 11:13:16 +0100

    don

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 08:25:20 by oait-si-          #+#    #+#             */
-/*   Updated: 2024/11/29 10:26:33 by oait-si-         ###   ########.fr       */
+/*   Updated: 2024/11/29 11:27:50 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,15 +37,56 @@ char    *ft_strchr(char *s, int c)
     return NULL;
 }
 
-chat    *ft_strjoin(char *s1, char *s2)
+static int  ft_strjoin_lenght(char  const *s1, char const   *s2)
+{
+    size_t  len_s1;
+    size_t  len_s2;
+    
+    len_s1 = 0;
+    len_s2 = 0;
+    if(s1)
+        len_s1 = ft_strlen(s1);
+    if(s2)
+        len_s2 = ft_strlen(s2);
+    return(len_s1 + len_s2); 
+}
+
+static void ft_strjoin_contact(char *new, char const *s1, char const *s2)
 {
     size_t  i;
     size_t  j;
-    char    *str;
-
-    if(!s1)
+    
+    i = 0;
+    if(s1)
     {
-        s1 =(char *)malloc(1 * sizeof(char));
-        
+        while(s1[i])
+        {
+            new[i] = s1[i];
+            i++;
+        }
     }
+    j = 0;
+    if(s2)
+    {
+        while(s2[j])
+        {
+            new[i] = s2[j];
+            i++;
+            j++;
+        }
+    }
+    new[i] = '\0';
+}
+ 
+char    *ft_strjoin(const char *s1, const char *s2)
+{
+    char    *new;
+    int     total_len;
+
+    total_len = ft_strjoin_lenght(s1, s2);
+    new = (char *)malloc(sizeof(char)*(total_len +1));
+    if(!new)
+        return (NULL);
+    ft_strjoin_contact(new, s1, s2);
+    return(new);
 }
